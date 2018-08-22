5f95f8efc3ac1f2207750184cd90fbb243e9f202
tanyp <652630339@qq.com>
2018-08-22 18:08:19 +0800

    default

--- js/index.js
+++ js/index.js
@@ -2,9 +2,9 @@ var app = angular.module('app', ['ui.bootstrap', 'ui.router', 'ngAnimate', 'quie
 app.controller('indexCtrl', function($scope, $http, $uibModal, $log) {
 
 
-	$scope.quiet = {
+    $scope.quiet = {
         images: [],
-        files:[]
+        files: []
     }
 
 
@@ -13,42 +13,29 @@ app.controller('indexCtrl', function($scope, $http, $uibModal, $log) {
     fileInput.addEventListener("change", function(evt) {
         var files = fileInput.files;
         for (var i = 0; i < files.length; i++) {
-        	files[i].objectURL = window.URL.createObjectURL(fileInput.files[i]);
-        	$scope.quiet.files.push(files[i]);
+            files[i].objectURL = window.URL.createObjectURL(fileInput.files[i]);
+            $scope.quiet.files.push(files[i]);
         }
-        fileInput.setAttribute("type","text");
+        fileInput.setAttribute("type", "text");
         $scope.$apply();
-        fileInput.setAttribute("type","file");
+        fileInput.setAttribute("type", "file");
+        console.log($scope.quiet.files);
+
     }, false);
 
     $scope.upload = function() {
         $("#fileChooser").click();
     }
 
-    $scope.fileUpload = function() {
-
-        console.log($scope.quiet.wx);
-
-        var files = document.querySelector('input[name="fileChooser"]').files;
-        for (var i = 0; i < files.length; i++) {
-            $scope.quiet.images.push(files[i]);
-        }
-    }
-
     $scope.release = function(e) {
-
-    	var files = $scope.quiet.files;
-
-    	console.log(files);
-
-    	var f = new window.FormData();
-
-    	for (var i = 0; i <files.length; i++) {
-    		f.append('file', files[i], files[i].name);
-    	}
-
-    	$.ajax({
-            url:'/tanyp/media/upload',
+        var files = $scope.quiet.files;
+        var f = new window.FormData();
+        for (var i = 0; i < files.length; i++) {
+            console.log(files[i].size);
+            f.append('file', files[i], files[i].name);
+        }
+        $.ajax({
+            url: '/tanyp/media/upload',
             type: 'POST',
             data: f,
             async: false,
@@ -64,6 +51,57 @@ app.controller('indexCtrl', function($scope, $http, $uibModal, $log) {
         });
     }
 
+    $scope.login = function() {
+        var modalInstance = $uibModal.open({
+            animation: true,
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: '/html/model/login.html', //script标签中定义的id
+            controller: 'QuietModalCtrl',
+            // controllerAs: '$ctrl',
+            size: 600,
+            // appendTo: parentElem,
+            resolve: {
+                items: function() {
+                    return $scope.items;
+                }
+            }
+        });
+        modalInstance.result.then(function(selectedItem) {
+            $log.info(selectedItem);
+        }, function() {
+            $log.info('Modal dismissed at: ' + new Date());
+        }).catch(console.error);
+    }
 
+    $scope.register = function() {
+        var modalInstance = $uibModal.open({
+            animation: true,
+            ariaLabelledBy: 'modal-title',
+            ariaDescribedBy: 'modal-body',
+            templateUrl: '/html/model/register.html',
+            controller: 'QuietModalCtrl',
+            size: 'sm',
+            resolve: {
+                items: function() {
+                    return $scope.items;
+                }
+            }
+        });
+        modalInstance.result.then(function(selectedItem) {
+            $log.info(selectedItem);
+        }, function() {
+            $log.info('Modal dismissed at: ' + new Date());
+        }).catch(console.error);
+    }
+});
+app.controller('QuietModalCtrl', function($scope, $uibModalInstance, items) {
 
+    $scope.ok = function() {
+        $uibModalInstance.close();
+    };
+
+    $scope.cancel = function() {
+        $uibModalInstance.dismiss('cancel');
+    };
 });
