9b3b86c36269b3e8c1b85f24fd91efa7e44aed0f
tanyp <652630339@qq.com>
2018-08-23 21:13:34 +0800

    default

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
 var app = angular.module('app', ['ui.bootstrap', 'ui.router', 'ngAnimate', 'quiet.directive']);
 app.controller('indexCtrl', function($scope, $http, $uibModal, $log) {
 
-
     $scope.quiet = {
         images: [],
         files: []
     }
 
-
     var fileInput = document.querySelector("#fileChooser");
     var holder = document.getElementById('holder');
+
     fileInput.addEventListener("change", function(evt) {
         var files = fileInput.files;
         for (var i = 0; i < files.length; i++) {
@@ -57,10 +56,10 @@ app.controller('indexCtrl', function($scope, $http, $uibModal, $log) {
             ariaLabelledBy: 'modal-title',
             ariaDescribedBy: 'modal-body',
             templateUrl: '/html/model/login.html', //script标签中定义的id
-            controller: 'QuietModalCtrl',
+            controller: 'LoginModalCtrl',
             // controllerAs: '$ctrl',
-            size: 600,
             // appendTo: parentElem,
+            size: 'xss',
             resolve: {
                 items: function() {
                     return $scope.items;
@@ -68,9 +67,9 @@ app.controller('indexCtrl', function($scope, $http, $uibModal, $log) {
             }
         });
         modalInstance.result.then(function(selectedItem) {
-            $log.info(selectedItem);
+            // $log.info(selectedItem);
         }, function() {
-            $log.info('Modal dismissed at: ' + new Date());
+            // $log.info('Modal dismissed at: ' + new Date());
         }).catch(console.error);
     }
 
@@ -80,8 +79,8 @@ app.controller('indexCtrl', function($scope, $http, $uibModal, $log) {
             ariaLabelledBy: 'modal-title',
             ariaDescribedBy: 'modal-body',
             templateUrl: '/html/model/register.html',
-            controller: 'QuietModalCtrl',
-            size: 'sm',
+            controller: 'RegisterModalCtrl',
+            size: 'xss',
             resolve: {
                 items: function() {
                     return $scope.items;
@@ -89,16 +88,115 @@ app.controller('indexCtrl', function($scope, $http, $uibModal, $log) {
             }
         });
         modalInstance.result.then(function(selectedItem) {
-            $log.info(selectedItem);
+            // $log.info(selectedItem);
         }, function() {
-            $log.info('Modal dismissed at: ' + new Date());
+            // $log.info('Modal dismissed at: ' + new Date());
         }).catch(console.error);
     }
 });
-app.controller('QuietModalCtrl', function($scope, $uibModalInstance, items) {
 
-    $scope.ok = function() {
-        $uibModalInstance.close();
+app.controller('LoginModalCtrl', function($scope, $uibModalInstance, items) {
+
+
+    $scope.login = function() {
+        if ($scope.email) {
+            var reg = new RegExp("^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$");
+            if (!reg.test($scope.email)) {
+                var email = document.getElementById('login-email');
+                email.style.border = "1px solid red";
+            } else {
+                if ($scope.pwd) {
+                    if ($scope.pwd.length >= 6) {
+                        $uibModalInstance.close();
+                    } else {
+                        var pwd = document.getElementById('login-pwd');
+                        pwd.style.border = "1px solid red";
+                    }
+                } else {
+                    var pwd = document.getElementById('login-pwd');
+                    pwd.style.border = "1px solid red";
+                }
+            }
+        } else {
+            var email = document.getElementById('login-email');
+            email.style.border = "1px solid red";
+        }
+    };
+
+    $scope.cancel = function() {
+        $uibModalInstance.dismiss('cancel');
+    };
+});
+
+app.controller('RegisterModalCtrl', function($scope, $uibModalInstance, items, $http) {
+
+    $scope.sendCode = function(email) {
+        var e = document.getElementById('register-email');
+        if (email) {
+            var reg = new RegExp("^[a-z0-9]+([._\\-]*[a-z0-9])*@([a-z0-9]+[-a-z0-9]*[a-z0-9]+.){1,63}[a-z0-9]+$");
+            if (!reg.test(email)) {
+                e.style.border = "1px solid red";
+            } else {
+                e.style.border = "1px solid #027fff";
+                $http.post('/tanyp/send/mail?email=' + email).then(function(res) {
+                    res = res.data;
+                    if (res.ret) {
+                        var code = document.querySelector("#register-code");
+                        code.removeAttribute("disabled");
+                        $scope.sendNum();
+                    }
+                });
+            }
+        } else {
+            e.style.border = "1px solid red";
+        }
+    }
+    var countdown = 60;
+    $scope.sendNum = function() {
+        var fileInput = document.querySelector(".send-code");
+        if (countdown == 0) {
+            fileInput.removeAttribute("disabled");
+            fileInput.innerHTML = "获取校验码";
+            countdown = 60;
+        } else {
+            fileInput.setAttribute("disabled", true);
+            fileInput.innerHTML = "重新发送(" + countdown + ")";
+            countdown--;
+            setTimeout(function() {
+                $scope.sendNum();
+            }, 1000)
+        }
+    }
+
+
+    $scope.register = function() {
+
+        if ($scope.email) {
+            if ($scope.password && $scope.password.length >=6) {
+                if ($scope.checkCode) {
+                    var user = {
+                        "email": $scope.email,
+                        "pwd": $scope.password,
+                        "code": $scope.checkCode
+                    };
+                    $http.post('/tanyp/u/register', user).then(function(res) {
+                        res = res.data;
+                        if (res.ret) {
+                            countdown = 0;
+                            $uibModalInstance.close();
+                        } else {
+
+                        }
+                    });
+                } else {
+                    document.querySelector("#register-code").style.border = "1px solid red";
+                }
+            } else {
+                document.querySelector("#register-password").style.border = "1px solid red";
+            }
+        } else {
+            document.querySelector("#register-email").style.border = "1px solid red";
+        }
     };
 
     $scope.cancel = function() {
